c6f60651149e5deb5cf90b2ba759eb26e2bcc6fd
raoul-van-rueschen-14-fhb <counaifr@wegwerfemail.de>
2014-12-06 13:24:28 +0100

    Added a test for adding an anime.

--- test/ahkneemay_test.js
+++ test/ahkneemay_test.js
@@ -4,24 +4,41 @@
 
 "use strict";
 
-var ahkneemay = require("../lib/ahkneemay.js");
+var ahkneemay;
 
 module.exports = {
  setUp: function(done)
  {
+  ahkneemay = require("../lib/ahkneemay.js");
   done();
  },
  tearDown: function(done)
  {
   done();
  },
- "setup aws": function(test)
+ test1: function(test)
  {
   test.expect(1);
 
   ahkneemay.init(true, "ahkneemay", function(result)
   {
-   test.equal(result, null, "AWS initialization should run without errors.");
+   test.equal(result, null, "aws initialization should finish without errors under optimal conditions.");
+   test.done();
+  });
+ },
+ test2: function(test)
+ {
+  var anime = {
+   title: "dummy",
+   author: "dummy",
+   year: 2000,
+   publisher: "dummy",
+   img: {}
+  };
+
+  ahkneemay.addAnime(anime, {}, function(result)
+  {
+   test.equal(result, null, "adding an anime should not yield any errors under optimal conditions.");
    test.done();
   });
  }
